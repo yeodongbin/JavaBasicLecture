4d10e8879fe875025f09ecd89bacd9b07e0fc01f
yeodongbin <yeodongbin2011@gmail.com>
2018-03-21 13:08:26 +0900

    2018-03-21 13:08

--- C.threads/src/threadStateControl/wait_notify/ThreadA.java
+++ C.threads/src/threadStateControl/wait_notify/ThreadA.java
@@ -9,7 +9,7 @@ public class ThreadA extends Thread {
 
 	@Override
 	public void run() {
-		for(int i=0; i<10; i++) {
+		for(int i=0; i<3; i++) {
 			workObject.methodA();
 		}
 	}
--- C.threads/src/threadStateControl/wati_notify2/DataBox.java
+++ C.threads/src/threadStateControl/wati_notify2/DataBox.java
@@ -4,10 +4,11 @@ public class DataBox {
 	private String data;
 
 	public synchronized String getData() {
-		if(this.data == null) {
+		if (this.data == null) {
 			try {
 				wait();
-			} catch(InterruptedException e) {}
+			} catch (InterruptedException e) {
+			}
 		}
 		String returnValue = data;
 		System.out.println("ConsummerThread가 읽은 데이터: " + returnValue);
@@ -17,15 +18,14 @@ public class DataBox {
 	}
 
 	public synchronized void setData(String data) {
-		if(this.data != null) {
+		if (this.data != null) {
 			try {
 				wait();
-			} catch(InterruptedException e) {}
+			} catch (InterruptedException e) {
+			}
 		}
 		this.data = data;
 		System.out.println("ProducerThread가 생성한 데이터: " + data);
 		notify();
 	}
 }
-
-
