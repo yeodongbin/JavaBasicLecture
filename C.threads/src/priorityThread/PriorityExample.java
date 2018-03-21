85ebde56846d79cd4b673a87ea9f664bef6cc7c8
yeodongbin <yeodongbin2011@gmail.com>
2018-03-21 09:39:15 +0900

    2018-03-21

---
+++ C.threads/src/priorityThread/PriorityExample.java
@@ -0,0 +1,31 @@
+package priorityThread;
+
+class CalcThread extends Thread {
+	public CalcThread(String name) {
+		setName(name);
+	}
+
+	public void run() {
+		for (int i = 0; i < 2000000000; i++) {
+		}
+		System.out.println(getName());
+	}
+}
+
+public class PriorityExample {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++) {
+			Thread thread = new CalcThread("thread" + i);
+
+			if (i != 10) {
+				thread.setPriority(Thread.MIN_PRIORITY);
+			} else {
+				thread.setPriority(Thread.MAX_PRIORITY);
+			}
+			thread.start();
+		}
+
+	}
+
+}
