12019e6d9241a02623b6bcf522c1dac7301a1c57
yeodongbin <yeodongbin2011@gmail.com>
2018-03-22 10:54:45 +0900

    2018-03-22 10:54

--- E.collectionFramework/src/map/HashtableExample.java
+++ E.collectionFramework/src/map/HashtableExample.java
@@ -1,18 +1,21 @@
 package map;
+
 import java.util.*;
 
 public class HashtableExample {
 
 	public static void main(String[] args) {
 		Map<String, String> map = new Hashtable<String, String>();
-		
+
 		map.put("spring", "12");
-		
+
+		System.out.print("ID, Password를 입력하시오. : ");
 		Scanner sc = new Scanner(System.in);
-		
-		while (true) {	
+
+		while (true) {
 			String id = sc.nextLine();
 			String password = sc.nextLine();
+
 			if (map.containsKey(id)) {
 				if (map.get(id).equals(password)) {
 					System.out.println("login");
@@ -24,5 +27,5 @@ public class HashtableExample {
 				System.out.println("no id");
 			}
 		}
-	} //main
+	} // main
 }
