733b40f0471d5675c5dc113369020b8b4c0f4d2f
yeodongbin <yeodongbin2011@gmail.com>
2018-11-10 22:41:03 +0900

    2018 11 10

---
+++ H.JavaSwing/src/javaswing_tutorial/ChoiceEx1.java
@@ -0,0 +1,54 @@
+package javaswing_tutorial;
+
+import java.awt.Choice;
+import java.awt.List;
+
+import javax.swing.JFrame;
+
+public class ChoiceEx1 {
+
+	public static void main(String[] args) {
+		JFrame jfr = new JFrame("Choice -Select");
+		jfr.setSize(500, 500);
+		jfr.setLayout(null); //사용자 임의 설정
+		
+		Choice day = new Choice();
+		day.add("Sun");
+		day.add("Mon");
+		day.add("Tur");
+		day.add("Wed");
+		day.add("Thu");
+		day.add("Fri");
+		day.add("Sat");
+		day.setSize(100,50);
+		day.setLocation(100, 70);
+		
+		List selectOne = new List(6); //default 4
+		selectOne.setLocation(100, 150);
+		selectOne.setSize(100,200);
+		selectOne.add("Sunday");
+		selectOne.add("Monday");
+		selectOne.add("Turesday");
+		selectOne.add("Wednesday");
+		selectOne.add("Thusday");
+		selectOne.add("Friday");
+		selectOne.add("Saturday");
+		
+		List selectMany = new List(6,true);
+		selectMany.setLocation(250, 150);
+		selectMany.setSize(50,200);
+		selectMany.add("Sunday");
+		selectMany.add("Monday");
+		selectMany.add("Turesday");
+		selectMany.add("Wednesday");
+		selectMany.add("Thusday");
+		selectMany.add("Friday");
+		selectMany.add("Saturday");
+		
+		jfr.add(day);
+		jfr.add(selectOne);
+		jfr.add(selectMany);
+		jfr.setVisible(true);
+	}
+
+}
--- H.JavaSwing/src/javaswing_tutorial/PanelButtonEx1.java
+++ H.JavaSwing/src/javaswing_tutorial/PanelButtonEx1.java
@@ -1,36 +1,71 @@
 package javaswing_tutorial;
 
 import java.awt.Container;
+import java.awt.Dimension;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
-class LaunchJFrame extends JFrame {
+class LaunchJFrame2 extends JFrame {
 	//construction
-	public LaunchJFrame() {
+	public LaunchJFrame2() {
 		super("First JFrame");
-		setBounds(200,200,500,300);//위치 크기
+		setBounds(200,200,700,500);//위치 크기
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		Container contentPane = this.getContentPane();
 		JPanel jpanel = new JPanel();
 		JButton btn = new JButton("Start");
+		JTextField textfield = new JTextField(10);
+		JLabel label = new JLabel(" Input Period : "); 
+		JCheckBox checkbox = new JCheckBox("Fire randomly");
+		
+		checkbox.addChangeListener(new ChangeListener() {
+			
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				if(((JCheckBox) e.getSource()).isSelected())
+				{
+					textfield.setText("Random");
+					textfield.setEnabled(false);
+				} else
+				{
+					textfield.setText("");
+					textfield.setEnabled(true);
+				}
+			}
+		});
+		
 		btn.setMnemonic('B');
-		btn.setSize(100,50);
+		btn.setSize(200,100);
+		btn.setPreferredSize(new Dimension(100, 50));
+		
+		label.setPreferredSize(new Dimension(100, 50));
+		textfield.setPreferredSize(new Dimension(100, 50));
 		
 		jpanel.add(btn);
-		add(jpanel);
-		setVisible(true);
+		jpanel.add(label);
+		jpanel.add(textfield);
+		jpanel.add(checkbox);
+//		add(jpanel); //2
+		contentPane.add(jpanel);
+		contentPane.setVisible(true);
 	}
 }
 
 public class PanelButtonEx1 {
-
+	
 	public static void main(String[] args) {
-
-		LaunchJFrame jframe = new LaunchJFrame();
+		LaunchJFrame2 jframe = new LaunchJFrame2();
 		jframe.setLocale(null);
 		jframe.setVisible(true);
-		
 	}
+	
 }
+
