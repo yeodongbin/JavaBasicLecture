56736b03b8f0fa74787af19fc87b1aad381c8612
yeodongbin <yeodongbin2011@gmail.com>
2018-11-12 10:35:23 +0900

    2018-11-12

---
+++ H.JavaSwing/src/javaswing_tutorial/Layout.java
@@ -0,0 +1,107 @@
+package javaswing_tutorial;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+class FlowLayoutEx extends JFrame {
+	FlowLayoutEx(){
+		setTitle("FlowLayout ");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//contentpane flowlayout left sorting, left-right 30, up-down 40
+		setLayout(new FlowLayout(FlowLayout.LEFT,30, 40));
+		
+		add(new JButton("1"));
+		add(new JButton("2"));
+		add(new JButton("3"));
+		add(new JButton("4"));
+		add(new JButton("5"));
+		
+		setSize(300, 200);
+		setVisible(true);
+	}
+}
+
+class BorderLayoutEx extends JFrame {
+	BorderLayoutEx(){
+		setTitle("BorderLayoutEx ");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		setLayout(new BorderLayout());
+		
+		add(new JButton("1"),BorderLayout.NORTH);
+		add(new JButton("2"),BorderLayout.CENTER);
+		add(new JButton("3"),BorderLayout.SOUTH);
+		add(new JButton("4"),BorderLayout.EAST);
+		add(new JButton("5"),BorderLayout.WEST);
+		
+		setSize(300, 200);
+		setVisible(true);
+		
+	}
+}
+
+class GridLayoutEx extends JFrame {
+	GridLayoutEx(){
+		setTitle("GridLayoutEx ");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		GridLayout gridLayout =new GridLayout(4,2); 
+		gridLayout.setVgap(5);
+		setLayout(gridLayout);
+		
+		add(new JLabel(" name :"));
+		add(new JTextField(""));
+		add(new JLabel(" student number :"));
+		add(new JTextField(""));
+		add(new JLabel(" department :"));
+		add(new JTextField(""));
+		add(new JLabel(" subject :"));
+		add(new JTextField(""));
+	
+		setSize(300, 200);
+		setVisible(true);
+		
+	}
+}
+
+class NullContainerEx extends JFrame {
+	NullContainerEx(){
+		setTitle("NullContainerEx ");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		setLayout(null);
+		
+		JLabel jla = new JLabel("Buttons");
+		jla.setLocation(130, 50);
+		jla.setSize(200, 20);
+		add(jla);
+		
+		for (int i=1;i<=9;i++) {
+			JButton btn = new JButton(Integer.toString(i));
+			btn.setLocation(i*15, i*15);
+			btn.setSize(50,20);
+			add(btn);
+		}
+	
+		setSize(300, 200);
+		setVisible(true);
+		
+	}
+}
+
+
+public class Layout {
+
+	public static void main(String[] args) {
+		new FlowLayoutEx();
+		new BorderLayoutEx();
+		new GridLayoutEx();
+		new NullContainerEx();
+	}
+}
---
+++ H.JavaSwing/src/javaswing_windowbuilder/MapViewer.java
@@ -0,0 +1,66 @@
+package javaswing_windowbuilder;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class MapViewer {
+
+	private JFrame frame;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MapViewer window = new MapViewer();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MapViewer() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 551, 407);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(17, 72, 495, 264);
+		frame.getContentPane().add(panel);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton.setBounds(346, 15, 125, 29);
+		frame.getContentPane().add(btnNewButton);
+		
+		textField = new JTextField();
+		textField.setBounds(122, 16, 207, 29);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+	}
+}
