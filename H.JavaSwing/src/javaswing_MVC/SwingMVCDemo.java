d3ce87a84faa679d23a48941846c7c51161f31c6
yeodongbin <yeodongbin2011@gmail.com>
2018-11-13 00:22:21 +0900

    2018-11-13 MVC pattern

---
+++ H.JavaSwing/src/javaswing_MVC/SwingMVCDemo.java
@@ -0,0 +1,21 @@
+package javaswing_MVC;
+
+import javax.swing.SwingUtilities;
+
+public class SwingMVCDemo {
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					createAndShowGUI();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public static void createAndShowGUI() throws Exception {
+		new View();
+	}
+}
---
+++ H.JavaSwing/src/javaswing_tutorial/SwingTest.java
@@ -0,0 +1,18 @@
+package javaswing_tutorial;
+
+import java.awt.Dimension;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+class SwingTest{
+    public static void main(String args[]){
+       JFrame frame = new JFrame("My First GUI");
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       frame.setSize(400,400);
+       JButton button = new JButton("Press");
+      
+       frame.getContentPane().add(button); // Adds Button to content pane of frame
+       frame.setVisible(true);
+    }
+}
