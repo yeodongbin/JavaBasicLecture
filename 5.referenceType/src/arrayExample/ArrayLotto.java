7e8a98b4e6f71be9bcac5a0c99b7b6a34ecf688c
Yeo, Dongbin <yeodongbin2011@gmail.com>
2020-04-22 15:40:49 +0900

    Array sample code update 2020-04-22

--- 5.referenceType/src/arrayExample/ArrayLotto.java
+++ 5.referenceType/src/arrayExample/ArrayLotto.java
@@ -3,8 +3,19 @@ package arrayExample;
 public class ArrayLotto {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] lotto = new int[6];
+		
+		int i,j;
+	       
+	       for(i=0; i<lotto.length; i++){
+	          lotto[i] = (int)((Math.random()*45)+1);
+	          if(i>0){
+	             for(j=0; j<i; j++){
+	                while(lotto[i]==lotto[j]){
+	                   lotto[i] = (int)((Math.random()*45)+1);
+	                }
+	             }
+	          }
+	       }
 	}
-
 }
--- 5.referenceType/src/problems/ArrayOperation.java
+++ 5.referenceType/src/problems/ArrayOperation.java
@@ -1,9 +1,60 @@
 package problems;
 
+import java.util.Scanner;
+
 public class ArrayOperation {
-
+	
 	public static void main(String[] args) {
-
+		   Scanner scan = new Scanner(System.in);
+	        
+	        System.out.println("첫번째 행렬 크기 입력");
+	        int x = scan.nextInt();
+	        int y = scan.nextInt();
+	        
+	        System.out.println("두번째 행렬 크기 입력");
+	        int x2 = scan.nextInt();
+	        int y2 = scan.nextInt();
+	        
+	        int[][] arr = new int[x][y];
+	        int[][] arr2 = new int[x2][y2];
+	        
+	        int[][] arr3 = new int[x][y2];
+	        
+	        
+	        System.out.println("첫번째 행렬 값 입력");
+	        for(int i=0; i<x; i++){
+	            for(int j=0; j<y; j++){
+	                arr[i][j] = scan.nextInt();
+	            }
+	        }
+	        
+	        System.out.println("두번째 행렬 값 입력");
+	        for(int i=0; i<x2; i++){
+	            for(int j=0; j<y2; j++){
+	                arr2[i][j] = scan.nextInt();
+	            }
+	        }
+	        
+	        //행렬곱셈
+	        for(int i=0; i<x; i++){
+	            for(int j=0; j<y2; j++){
+	                int sum = 0;
+	                for(int k=0; k<y; k++){
+	                    sum+=arr[i][k]*arr2[k][j];
+	                }
+	                arr3[i][j] = sum;
+	            }            
+	        }
+	        
+	        
+	        
+	        //출력
+	        for(int i=0; i<x; i++){
+	            for(int j=0; j<y2; j++){
+	                System.out.print(arr3[i][j] + " ");
+	            }
+	            System.out.println();
+	        }
 	}
 
 }
