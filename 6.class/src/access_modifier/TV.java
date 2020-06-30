72e92dd47a8cb93c44be0ff8c05d7d39bd4abb2a
Yeo, Dongbin <yeodongbin2011@gmail.com>
2020-06-30 10:17:01 +0900

    2020 06 30 접근 제한자 설명 코드

---
+++ 6.class/src/access_modifier/TV.java
@@ -0,0 +1,106 @@
+package accessmodifier.tv;
+
+public class TV {
+	private boolean power = false;
+	boolean mute = false;
+	private int tempVol = 0;
+	private int channal = 7;
+	private int volume = 3;
+	final int maxVol = 20;
+	final int minVol = 0;
+	final int maxCh = 100;
+	final int minCh = 1;
+
+	void PowerOn() {
+		power = true;
+		System.out.println("TV ON!!");
+	}
+
+	void PowerOff() {
+		power = false;
+		System.out.println("TV OFf!!!~");
+	}
+
+	int chUP() {
+		if (power == true) {
+			if (channal == maxCh) {
+				channal = minCh;
+			} else {
+				channal++;
+			}
+		}
+		System.out.println("channal : " + channal);
+		return channal;
+	}
+
+	int chDown() {
+		if (power == true) {
+			if (channal == minCh) {
+				channal = maxCh;
+			} else {
+				channal--;
+			}
+		}
+		System.out.println("channal : " + channal);
+		return channal;
+	}
+
+	int changeCh(int ch) {
+		if (power) {
+			if ((ch >= minCh) && (ch <= maxCh)) {
+				channal = ch;
+			} else {
+				System.out.println(ch + "은 적절한 입력이 아닙니다.");
+			}
+		}
+		System.out.println("channal : " + channal);
+		return channal;
+	}
+
+	int volUp() {
+		if (power) {
+			if (volume == maxVol) {
+				System.out.println("최대볼륨입니다.");
+			} else {
+				volume++;
+			}
+		}
+		System.out.println("Volume : " + volume);
+		return volume;
+	}
+
+	int volDown() {
+		if (power) {
+			if (volume == minVol) {
+				System.out.println("최소볼륨입니다.");
+			} else {
+				volume--;
+			}
+		}
+		System.out.println("Volume : " + volume);
+		return volume;
+	}
+
+	void setMute() {
+		if (power) {
+			if (!mute) {
+				mute = true;
+				tempVol = volume;
+				volume = 0;
+				System.out.println("음소거 입니다.");
+			} else {
+				mute = false;
+				volume = tempVol;
+				tempVol = 0;
+				System.out.println("음소거를 제거합니다.");
+			}
+		}
+	}
+
+	void check() {
+		if (power) {
+			System.out.println("Volume : " + volume);
+			System.out.println("channal : " + channal);
+		}
+	}
+}
---
+++ 6.class/src/access_modifier/TVExample.java
@@ -0,0 +1,18 @@
+package accessmodifier.tv;
+
+public class TVExample {
+
+	public static void main(String[] args) {
+		TV tv = new TV();
+		tv.PowerOn();
+		tv.check();
+		tv.changeCh(11);
+		tv.check();
+		tv.setMute();
+		tv.check();
+		
+		
+
+	}
+
+}
